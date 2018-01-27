959d5f3717fcc8c416d155045ad09a7359525b9b
a5720071a <a5720071a@gmail.com>
2018-01-27 11:48:19 +0700

    remove js hardcode

--- app/assets/javascripts/clients.js
+++ app/assets/javascripts/clients.js
@@ -1,14 +1,26 @@
-$(document).on('turbolinks:load', function(){
-  var box_height = $(window).height() / 2 - $("#home-content").height() / 2;
-  var box_width = $(window).width() / 2 - $("#home-content").width() / 2;
-  $("#home-content").css({position: "absolute"});
-  $("#home-content").css({top: box_height});
-  $("#home-content").css({left: box_width});
-});
+$(document).on("turbolinks:load",function(){
+  var content;
+  if ($("#home-content").length){
+    content = $("#home-content");
+  } else if ($("#upload-content").length){
+    content = $("#upload-content");
+  }
+  var box_height = $(window).height() / 2 - content.height() / 2;
+  var box_width = $(window).width() / 2 - content.width() / 2;
+  content.css({position: "absolute"});
+  content.css({top: box_height});
+  content.css({left: box_width});
+})
 $(window).on('resize', function(){
-  var box_height = $(window).height() / 2 - $("#home-content").height() / 2;
-  var box_width = $(window).width() / 2 - $("#home-content").width() / 2;
-  $("#home-content").css({top: box_height});
-  $("#home-content").css({left: box_width});
+  var content;
+  if ($("#home-content").length){
+    content = $("#home-content");
+  } else if ($("#upload-content").length){
+    content = $("#upload-content");
+  }
+  var box_height = $(window).height() / 2 - content.height() / 2;
+  var box_width = $(window).width() / 2 - content.width() / 2;
+  content.css({position: "absolute"});
+  content.css({top: box_height});
+  content.css({left: box_width});
 });
-
