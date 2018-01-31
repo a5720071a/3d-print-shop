8ecf3be7a147b7995f1556d8d9ebd728dd584015
a5720071a <a5720071a@gmail.com>
2018-01-31 13:35:37 +0700

    remove unused routes

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,7 +5,7 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params) 
     if @user.save
-    #  session[:user_id] = @user.id 
+      session[:user_id] = @user.id 
       redirect_to '/'
     else
       redirect_to '/users/new' 
--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,8 @@ Rails.application.routes.draw do
   get '/upload' => 'clients#upload'
   get '/browse' => 'clients#browse'
   get '/tutorial' => 'clients#tutorial'
-  resources :users
+  get '/signup' => 'users#new'
+  post '/signup' => 'users#create'
   get '/login' => 'sessions#new'
   post '/login' => 'sessions#create'
   get '/logout' => 'sessions#destroy'
