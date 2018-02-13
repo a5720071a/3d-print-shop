3ab5662dc2830fa5459b230a85a92b6143e02ea4
a5720071a <a5720071a@gmail.com>
2018-02-14 01:29:52 +0700

    validate uniqueness of username and email

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,7 @@
 class User < ApplicationRecord
   has_many :orders
   has_secure_password
-  validates :email, presence: true
-  validates :username, presence: true
+  validates :email, presence: true, uniqueness: true
+  validates :username, presence: true, uniqueness: true
   validates :password, presence: true
 end
