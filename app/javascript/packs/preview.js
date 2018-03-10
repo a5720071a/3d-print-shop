e1a94c888167e09ee974508d3dfb3e8e9ef91783
a5720071a <a5720071a@gmail.com>
2018-03-11 00:33:06 +0700

    auto calculate a scaled model size

--- app/javascript/packs/preview.js
+++ app/javascript/packs/preview.js
@@ -3,7 +3,7 @@ const STLLoader = require('three-stl-loader')(THREE)
 const OrbitControls = require('three-orbit-controls')(THREE)
 
 $(document).on("turbolinks:load", function() {
-  var model_url_container = $("#model_url");
+  var model_url_container = $("#model-url");
   if(model_url_container.length) {
 
     //if ( ! Detector.webgl ) Detector.addGetWebGLMessage();
@@ -33,13 +33,13 @@ $(document).on("turbolinks:load", function() {
       // create mesh
 	    var loader = new STLLoader();
 	    loader.load( model_url, function ( geometry ) {
+        getModelScale(geometry)
 		    var meshMaterial = new THREE.MeshPhongMaterial( { color: 0xAAAAAA, specular: 0x111111, shininess: 200 } );
 		    mesh = new THREE.Mesh( geometry, meshMaterial );
 		    mesh.position.set( 0, -0.5, 0 );
 		    mesh.rotation.set( 3 * Math.PI / 2 , 0, 0 );
         mesh.scale.set( 0.015, 0.015, 0.015 );
 		    scene.add( mesh );
-
 	    } );
 
 	    // create light source
@@ -78,23 +78,23 @@ $(document).on("turbolinks:load", function() {
 	    $("#model-color").on( 'change', function(e){
         mesh.material.color.setHex( this.value );
       });
-      $("#quality_ll").on( 'change', function(e){
+      $("#quality-ll").on( 'change', function(e){
         renderer.setSize( 150 , 150 , false );
         console.log("very low")
       });
-      $("#quality_l").on( 'change', function(e){
+      $("#quality-l").on( 'change', function(e){
         renderer.setSize( 200 , 200 , false );
         console.log("low")
       });
-      $("#quality_m").on( 'change', function(e){
+      $("#quality-m").on( 'change', function(e){
         renderer.setSize( 250 , 250 , false );
         console.log("medium")
       });
-      $("#quality_h").on( 'change', function(e){
+      $("#quality-h").on( 'change', function(e){
         renderer.setSize( 375 , 375 , false );
         console.log("high")
       });
-      $("#quality_hh").on( 'change', function(e){
+      $("#quality-hh").on( 'change', function(e){
         renderer.setSize( 500 , 500 , false );
         console.log("very high")
       });
@@ -107,6 +107,31 @@ $(document).on("turbolinks:load", function() {
 	    scene.add( directionalLight );
     }
 
+    function getModelScale(geometry) {
+      geometry.computeBoundingBox()
+      var boundingBox = geometry.boundingBox.clone();
+      var geo_width = Math.abs(boundingBox.min.x) + boundingBox.max.x;
+      var geo_depth = Math.abs(boundingBox.min.y) + boundingBox.max.y;
+      var geo_height = Math.abs(boundingBox.min.z) + boundingBox.max.z;
+      var min_dimension = Math.min(geo_width,geo_depth,geo_height);
+      var ratio_width = (geo_width / min_dimension).toFixed(2);
+      var ratio_depth = (geo_depth / min_dimension).toFixed(2);
+      var ratio_height = (geo_height / min_dimension).toFixed(2);
+      $('#model-scale').text("Scale - "+ratio_height+"(H) : "+ratio_width+"(W) : "+ratio_depth+"(D)")
+	    $("#print-depth").on( 'change', function(e){
+        $("#print-width").val((this.value / ratio_depth * ratio_width).toFixed(1));
+        $("#print-height").val((this.value / ratio_depth * ratio_height).toFixed(1));
+      });
+	    $("#print-width").on( 'change', function(e){
+        $("#print-depth").val((this.value / ratio_width * ratio_depth).toFixed(1));
+        $("#print-height").val((this.value / ratio_width * ratio_height).toFixed(1));
+      });
+	    $("#print-height").on( 'change', function(e){
+        $("#print-depth").val((this.value / ratio_height * ratio_depth).toFixed(1));
+        $("#print-width").val((this.value / ratio_height * ratio_width).toFixed(1));
+      });
+    }
+    
     function animate() {
 	    requestAnimationFrame( animate );
 	    render();
