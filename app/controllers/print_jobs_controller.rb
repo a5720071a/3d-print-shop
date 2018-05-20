4fe025cd97212906983ad7df67d984137e039977
a5720071a <a5720071a@gmail.com>
2018-05-20 15:16:15 +0700

    respond to json job request

--- app/controllers/print_jobs_controller.rb
+++ app/controllers/print_jobs_controller.rb
@@ -1,17 +1,17 @@
 class PrintJobsController < ApplicationController
   def request_jobs
-    @orders = Order.where(print_job_generated: false).joins(:payment).where(payments: {status: "Success"})
-    @item_list = @orders.joins(:items).select("items.id as item_id").map { |order| order.item_id }
+    #change to false
+    @orders = Order.where(print_job_generated: true).joins(:payment).where(payments: {status: 'Success'})
+    @item_list = @orders.joins(:items).select('items.id as item_id').map { |order| order.item_id }
     @items = Item.where id: @item_list
-    @jobs = @items.joins(:gcode).select("items.*, gcodes.filename as gcode_filename")
-    #@orders.each do |order|
-    #  order.print_job_generated = true
-    #  order.save!
-    #end
-    if not @jobs.empty?
-      send_data @jobs.to_json, :filename => 'jobs.JSON'
-    else
-      render status: '200'
+    @jobs = @items.joins(:gcode).select('items.id as item_id, gcodes.filename as gcode_filename')
+    @orders.each do |order|
+      order.print_job_generated = true
+      order.save!
+    end
+    respond_to do |format|
+      format.json { render json: @jobs.to_json( :only => [:item_id, :gcode_filename]) }
+      format.html { redirect_to root_path }
     end
   end
 end
